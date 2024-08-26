6c5a1f0115d42029c0141949a34af2efe51cf93f
thiru03karthik <thiru03karthik@gmail.com>
2024-08-26 13:37:01 +0530

    modified practice.py file

--- practice.py
+++ practice.py
@@ -1,18 +1,21 @@
 #Get input fornvariable name, score, department
 #Get score for 100
 #Divide 100/10
-
-# name=str(input())
-# score=int(input())
-# department=input()
+print("Enter your name")
+name=str(input())
+print("Enter your score")
+score=int(input())
+print("Enter your department")
+department=input()
+print ("output")
 # print("my name is",name)
 # print("my score is",score/10,"/10")
 # print("my department is",department)
 
 # print("my name is", name); print("my score is", score/10, "/10"); print("my department is", department)
-# print("my name is", cname, "\nmy score is", score/10, "/10", "\nmy department is", department)
+print("my name is", name, "\nmy score is", score/10, "/10", "\nmy department is", department)
 
-a=50
-b=50
-c=a+b
-print(100* str(c))
+# a=50
+# b=50
+# c=a+b
+# print(100*str(c))
