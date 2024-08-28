b2a76870c7e3e39a3bbc9690b2d3c5afe546bd5e
thiru03karthik <thiru03karthik@gmail.com>
2024-08-28 07:17:15 +0530

    modified dictionary.py and practice.py file

---
+++ dictionary.py
@@ -0,0 +1,27 @@
+# d1=() #tuple
+# d2={} #dict
+# d3=[] #list
+# print(type(d1))
+# print(type(d2))
+# print(type(d3))
+
+#dictionary type
+
+d1={"Laptop":"Macbook pro 13inch","Phone":"Iphone 13 128gb Blue","Headphone":"Airbuds pro 2"}
+#print(d1["Laptop"])
+
+#d1["karthik"]="digital marketer" #key & value added type
+#d3 =d1.copy()
+#del d1["Laptop"] #key & value deleted type
+#d3 =d1 #swap type
+
+#del d1["Phone"]
+#d3.update({"gideon": "content writer"})
+
+print("Enter the item name")
+key=input()
+print("product name",key,"is",d1[key])
+
+
+ 
+
