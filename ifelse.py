0aaa0b70c6e1e84890cf0b37ff9952e85d239750
thiru03karthik <thiru03karthik@gmail.com>
2024-08-31 07:04:56 +0530

    modified

---
+++ ifelse.py
@@ -0,0 +1,36 @@
+#question
+#get user input for variable meghna, if meghna==died print
+#surya meets priya"
+#else "suriya weds meghna"
+
+'''print("what happend for meghna?")
+meghna = input()
+if(meghna=="died"):
+    print("surya meets priya")
+else:
+    print("Surya weds Meghna")
+    
+#1
+#get input for variable mark. if mark>35 print pass. else print fail
+#sample input
+#mark:34
+
+print("whats is your mark?, I will tell pass or fail!")
+mark = int(input())
+if(mark>35):
+    print("Pass")
+else:
+    print("fail")
+
+#2
+#get input for variable income is greater than 7000 scholarship is available. else not eligible for scholarship
+#smaple input
+#income
+#Not eligible for scholarship"
+'''
+
+income =int(input("Enter the income: "))
+if(income>7000):
+    print("Not Eligible for Scholarship")
+else:
+    print("scholarship is available")
