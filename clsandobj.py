9f48bc18f2c3fab7f04f367f6f8101c22d78f04f
thiru03karthik <thiru03karthik@gmail.com>
2024-10-01 16:44:34 +0530

    updates

---
+++ clsandobj.py
@@ -0,0 +1,11 @@
+class goa:
+    drink=""
+    def party(self):
+        print("lets party")
+    def beach(self):
+        print("Enjoying the beach")
+
+remesh = goa()
+karthik = goa()
+
+remesh.party()
